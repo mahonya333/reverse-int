fed846c451d377eeddfaf309ec80201a1298cf06
Vladyslav Mahonia <mahonya333@gmail.com>
2022-07-02 03:44:45 +0300

    feat: add function reverse integer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let stringNumber = String(n);
+    let i = 0;
+    let result = "";
+
+    if (n < 0) {
+        while (i < stringNumber.length - 1) {
+            result = result + stringNumber[stringNumber.length - 1 - i];
+            i++;
+            console.log(result)
+        }
+    } else {
+        while (i < stringNumber.length) {
+            result = result + stringNumber[stringNumber.length - 1 - i];
+            i++;
+        }
+    }
+
+
+
+    result = Number(result);
+    return result;
+};
